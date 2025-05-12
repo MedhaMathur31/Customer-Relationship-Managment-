fa1888f6dcc680a49eaa753f077e250c91b92ea6
MedhaMathur31 <medha2004mathur@gmail.com>
2025-05-12 13:13:38 +0530

    updates

--- server/Index.js
+++ server/Index.js
@@ -17,7 +17,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://customer-relationship-managment.vercel.app',
   credentials: true
 }));
 
